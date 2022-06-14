ccb6f27a95561904358e26076097dcbf5f4c38f3
Kirigoulou <haouasyazid75@epita.fr>
2022-06-14 17:34:02 +0200

    Added stack data structure

---
+++ stack.c
@@ -0,0 +1,41 @@
+//
+// Created by kirig on 13/06/2022.
+//
+#include <stdio.h>
+#include "stdlib.h"
+#include "stack.h"
+
+Stack* init_stack() {
+    return NULL;
+}
+
+int st_is_empty(Stack* stack) {
+    return stack == NULL;
+}
+
+void* st_peek(Stack* stack) {
+    if (stack == NULL) {
+        fprintf(stderr, "cannot peek into an empty stack");
+        exit(1);
+    }
+
+    return stack->value;
+}
+
+void st_push(Stack** stack, void* elem) {
+    Stack* new_head = malloc(sizeof(Stack*));
+    new_head->value = elem;
+    new_head->next = *stack;
+    *stack = new_head;
+}
+
+void st_pop(Stack** stack) {
+    if (*stack == NULL){
+        fprintf(stderr, "cannot pop an empty stack");
+        exit(1);
+    }
+
+    Stack* target = *stack;
+    *stack = (**stack).next;
+    free(target);
+}
---
+++ stack.h
@@ -0,0 +1,25 @@
+//
+// Created by kirig on 13/06/2022.
+//
+
+#ifndef CALCULATOR_STACK_H
+#define CALCULATOR_STACK_H
+
+#include "types.h"
+
+typedef struct Stack {
+    void* next;
+    void* value;
+} Stack;
+
+Stack* init_stack();
+
+int st_is_empty(Stack* stack);
+
+void* st_peek(Stack* stack);
+
+void st_push(Stack** stack, void* elem);
+
+void st_pop(Stack** stack);
+
+#endif //CALCULATOR_STACK_H
