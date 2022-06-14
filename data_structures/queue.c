8b2897dd386c9dfed9caa3ad5f4104ed5b9d7e2c
Kirigoulou <haouasyazid75@epita.fr>
2022-06-14 18:04:03 +0200

    added queue data structure

--- data_structures/queue.c
+++ data_structures/queue.c
@@ -1,3 +1,45 @@
 //
 // Created by kirig on 14/06/2022.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+Queue* init_queue() {
+    return NULL;
+}
+
+int qu_is_empty(Queue* queue) {
+    return queue == NULL;
+}
+
+void* qu_peek(Queue* queue) {
+    if (qu_is_empty(queue)){
+        fprintf(stderr, "cannot peek an empty queue");
+        exit(1);
+    }
+
+    while (queue->next != NULL)
+        queue = queue->next;
+
+    return queue->value;
+}
+
+void qu_push(Queue** queue, void* elem) {
+    Queue* new_head = (Queue*) malloc(sizeof(Queue*));
+    new_head->next = *queue;
+    new_head->value = elem;
+    *queue = new_head;
+}
+
+void qu_pop(Queue** queue) {
+    if (qu_is_empty(*queue)) {
+        fprintf(stderr, "cannot pop an empty queue");
+        exit(1);
+    }
+
+    while ((**queue).next != NULL)
+        queue = (Queue**) &(**queue).next;
+
+    *queue = NULL;
+}
--- data_structures/queue.h
+++ data_structures/queue.h
@@ -5,4 +5,19 @@
 #ifndef CALCULATOR_QUEUE_H
 #define CALCULATOR_QUEUE_H
 
+typedef struct Queue {
+    void* next;
+    void* value;
+} Queue;
+
+Queue* init_queue();
+
+int qu_is_empty(Queue* queue);
+
+void* qu_peek(Queue* queue);
+
+void qu_push(Queue** queue, void* elem);
+
+void qu_pop(Queue** queue);
+
 #endif //CALCULATOR_QUEUE_H
