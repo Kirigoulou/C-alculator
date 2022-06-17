8efc3c18289a2c6a66e0461ebda4b601fe6b8348
Kirigoulou <haouasyazid75@epita.fr>
2022-06-17 23:58:38 +0200

    added binary tree to postfix expression function for the AST

--- data_structures/ast.c
+++ data_structures/ast.c
@@ -20,3 +20,17 @@ void infix_traversal(Node* root) {
         infix_traversal(root->right);
     }
 }
+
+void ast_to_postfix_rec(Queue** postfix_expr_ptr, Node* root) {
+    if (root != NULL) {
+        ast_to_postfix_rec(postfix_expr_ptr, root->left);
+        ast_to_postfix_rec(postfix_expr_ptr, root->right);
+        qu_push(postfix_expr_ptr, root->value);
+    }
+}
+
+Queue* ast_to_postfix(Node* root) {
+    Queue* postfix_expr = init_queue();
+    ast_to_postfix_rec(&postfix_expr, root);
+    return postfix_expr;
+}
--- data_structures/ast.h
+++ data_structures/ast.h
@@ -5,6 +5,8 @@
 #ifndef CALCULATOR_AST_H
 #define CALCULATOR_AST_H
 
+#include "queue.h"
+
 typedef struct Node {
     struct Node* left;
     struct Node* right;
@@ -14,5 +16,7 @@ typedef struct Node {
 Node* init_node(void* value, Node* left, Node* right);
 
 void infix_traversal(Node* root);
+
+Queue* ast_to_postfix(Node* root);
 
 #endif //CALCULATOR_AST_H
